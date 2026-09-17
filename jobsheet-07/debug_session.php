bdc43ee1badb55757224bc698a7428997630e569
dancelhal2000 <aobinaobin15@gmail.com>
2026-09-17 18:03:44 +0700

    feat: add debug_session.php to inspect session contents

---
+++ jobsheet-07/debug_session.php
@@ -0,0 +1,10 @@
+﻿<?php
+$page_title = "Debug Session";
+include __DIR__ . '/includes/header.php';
+?>
+        <section>
+            <h2>Debug Isi $_SESSION</h2>
+            <p>Halaman ini menampilkan isi mentah dari variabel superglobal <code>$_SESSION</code> di server saat ini:</p>
+            <pre><?php print_r($_SESSION); ?></pre>
+        </section>
+<?php include __DIR__ . '/includes/footer.php'; ?>
